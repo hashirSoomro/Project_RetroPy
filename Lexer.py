7783d422b3e31b80e30d971e107790e0ce345789
hashirSoomro <hashirsomroo@gmail.com>
2023-11-22 23:04:10 +0500

    Create Word Splitter and Tokenizer
    The word splitter and tokenizer is almost complete.

--- Lexer.py
+++ Lexer.py
@@ -1,53 +1,347 @@
 import re
 
 # Define token types
-TOKEN_TYPES = {
-    'DATATYPE':r'int|float|bool|double|str|char|byte|list|tuple|dict',# int a= | public int a= 
-    'GLOBAL':r'global|nonlocal|del', # global i, del i,nonlocal i |global(i),del(i),nonlocal(i)
-    'DEFINE':r'def|class', # def sum():,class sum(): | def sum(a,b): , class sum(a,b):
-    'VALUE':r'True|False|None', # = True
-    'LAMBDA':r'lambda', # lambda a, b, c : a + b + c
-    'ACCESSMODIFIER':r'public|private|protected|internal',# public string a='hey'
-    'FOR':r'for', #for x in fruits:|for x in range(0,2):
-    'IF':r'if|elif|while',# if(x>6): , while(a>6): | if x>6: , while a>6:
-    'EXCEPTIONHANDLING':r'try|else|finally',# try: ,else: ,finally:
-    'EXCEPT':r'except', # except: | except Exception:
-    'RAISE':r'raise', # raise TypeError("Only integers are allowed")
-    'ASSERT':r'assert',# assert x == "goodbye", "x should be 'hello'"
-    'SKIP':r'break|continue|pass', # break|continue|pass
-    'AS':r'as',# import calendar as c | with open('geek.txt') as geek:
-    'WITH':r'with', #with open('geek.txt') as geek:
-    'RETURN':r'return|yield', # return even_nums, yield i | return [num for num in numbers if not num % 2]
-    'ASYNC':r'async',# async def main():
-    'AWAIT':r'await',# await asyncio.gather(count(), count(), count())
-    'IMPORT':r'import|from',# import datetime | from datetime import time
+TOK_TYP = {
+    'DATATYPE':['int','float','bool','str','char','byte','list','tuple','dict'],# int a= | public int a= 
+    'GLOBAL':['global','nonlocal','del'], # global i, del i,nonlocal i |global(i),del(i),nonlocal(i)
+    'DEFINE':['def','class'], # def sum():,class sum(): | def sum(a,b): , class sum(a,b):
+    'VALUE':['True','False','None'], # = True
+    'LAMBDA':['lambda'], # lambda a, b, c : a + b + c
+    'ACCESSMODIFIER':['public','private','protected','internal'],# public string a='hey'
+    'FOR':['for'], #for x in fruits:|for x in range(0,2):
+    'IF':['if','elif','while'], # if(x>6): , while(a>6): | if x>6: , while a>6:
+    'EXCEPTIONHANDLING':['try','else','finally'],# try: ,else: ,finally:
+    'EXCEPT':['except'], # except: | except Exception:
+    'RAISE':['raise'], # raise TypeError("Only integers are allowed")
+    'ASSERT':['assert'],# assert x == "goodbye", "x should be 'hello'"
+    'SKIP':['break','continue','pass'], # break|continue|pass
+    'AS':['as'],# import calendar as c | with open('geek.txt') as geek:
+    'WITH':['with'], #with open('geek.txt') as geek:
+    'RETURN':['return','yield'], # return even_nums, yield i | return [num for num in numbers if not num % 2]
+    'ASYNC':['async'],# async def main():
+    'AWAIT':['await'],# await asyncio.gather(count(), count(), count())
+    'IMPORT':['import','from'],# import datetime | from datetime import time
     'IDENTIFIER': r'[a-zA-Z_][a-zA-Z0-9_]*', 
     'INTEGER_LITERAL': r'\d+',
     'FLOAT_LITERAL': r'\d+\.\d+',
     'STRING_LITERAL': r'\".*?\"|\'.*?\'',
-    'EXPONENT': r'^',
-    'ADD_SUB': r'+|-',
-    'MUL_DIV':r'*|/|//|%',
-    'ASSIGN_OPERATOR':r'=|+=|-=|*=|/=|%=|//=|^=',
-    'REL_OPERATOR':r'==|!=|<|>|<=|>=',
-    'AND':'and',
-    'OR':'or',
-    'NOT':r'not',
-    'IN':r'in',
-    'IS':r'is',    
-    '(':r'(',
-    ')':r')',
-    '[':r'[',
-    ']':r']',
-    '{':r'{',
-    '}':r'}',
-    ',':r',',
-    '.':r'.',
-    '_':r'_',
-    '...':r'...',
-    'COMMENT': r'#',
-    'WHITESPACE': r' |  |   ',
+    'EXPONENT': ['^'],
+    'ADD_SUB': ['+','-'],
+    'MUL_DIV':['*','/','//','%'],
+    'ASSIGN_OPERATOR':['=','+=','-=','*=','/=','%=','^='],
+    'REL_OPERATOR':['==','!=','<','>','<=','>='],
+    'AND':['and'],
+    'OR':['or'],
+    'NOT':['not'],
+    'IN':['in'],
+    'IS':['is'],    
+    '(':['('],
+    ')':[')'],
+    '[':['['],
+    ']':[']'],
+    '{':['{'],
+    '}':['}'],
+    ',':[','],
+    '.':['.'],
+    ':':[':'],
+    ';':[';'],
+    '"':['"'],
+    "'":["'"],
+    '$':['$'],
+    '!':['!'],
+    'COMMENT':['#'],
+    'WHITE_SPACE': [' ','  ','   '],
+    'ESC_SEQ':['\n','\t']
 }
+
+def tokenizer(words):
+    tokens=[]
+    line_no = 1
+    print("Class Part Value Part Line Number")
+    for i in range(len(words)):
+        if words[i] in TOK_TYP['DATATYPE']:
+            tok=['DATATYPE',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+            
+        elif words[i] in TOK_TYP['GLOBAL']:
+            tok=['GLOBAL',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['DEFINE']:
+            tok=['DEFINE',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['VALUE']:
+            tok=['VALUE',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['LAMBDA']:
+            tok=['LAMBDA',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+            
+        elif words[i] in TOK_TYP['ACCESSMODIFIER']:
+            tok=['ACCESSMODIFIER',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['FOR']:
+            tok=['FOR',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['IF']:
+            tok=['IF',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['EXCEPTIONHANDLING']:
+            tok=['EXCEPTIONHANDLING',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['EXCEPT']:
+            tok=['EXCEPT',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['RAISE']:
+            tok=['RAISE',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['ASSERT']:
+            tok=['ASSERT',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['SKIP']:
+            tok=['SKIP',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['AS']:
+            tok=['AS',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['WITH']:
+            tok=['WITH',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['RETURN']:
+            tok=['RETURN',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['ASYNC']:
+            tok=['ASYNC',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['AWAIT']:
+            tok=['AWAIT',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['IMPORT']:
+            tok=['IMPORT',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['EXPONENT']:
+            tok=['EXPONENT',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['ADD_SUB']:
+            tok=['ADD_SUB',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['MUL_DIV']:
+            tok=['MUL_DIV',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['ASSIGN_OPERATOR']:
+            tok=['ASSIGN_OPERATOR',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['REL_OPERATOR']:
+            tok=['REL_OPERATOR',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['AND']:
+            tok=['AND',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['OR']:
+            tok=['OR',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['NOT']:
+            tok=['IN',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['IS']:
+            tok=['IS',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['(']:
+            tok=['(',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP[')']:
+            tok=[')',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['[']:
+            tok=['[',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP[']']:
+            tok=[']',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['{']:
+            tok=['{',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['}']:
+            tok=['}',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP[',']:
+            tok=[',',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['.']:
+            tok=['.',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP[':']:
+            tok=[':',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+            
+        elif words[i] in TOK_TYP[';']:
+            tok=[';',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i][0]=='"':
+            tok=['"',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i][0]=="'":
+            tok=["'",words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['$']:
+            tok=['$',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['!']:
+            tok=['!',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+        elif words[i] in TOK_TYP['COMMENT']:
+            tok=['COMMENT',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+            
+        elif words[i] in TOK_TYP['WHITE_SPACE']:
+            tok=['WHITE_SPACE',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+            
+        elif words[i] in TOK_TYP['ESC_SEQ']:
+            if words[i]=='\n':
+                tok=['ESC_SEQ',words[i],line_no]
+                tokens.append(tok)
+                #print(tokens,i)
+                line_no=line_no+1
+                i=i+1
+            else:
+                tok=['ESC_SEQ',words[i],line_no]
+                tokens.append(tok)
+                #print(tokens,i)
+                i=i+1
+        else:
+            tok=['NORM',words[i],line_no]
+            tokens.append(tok)
+            #print(tokens,i)
+            i=i+1
+
+    return tokens
 
 def word_splitter(source_code):
     words=[]
@@ -60,9 +354,9 @@ def word_splitter(source_code):
     white_space=['    ','   ','  ',' ']
     
     global i
-    line_number = 0
     i=0
     lexem=""
+    
     while(i<len(source_code)):
         if(source_code[i] in white_space):
             if(lexem==""):
@@ -99,10 +393,52 @@ def word_splitter(source_code):
                     #print(i,words,"single_op")
                 
         elif (source_code[i] in punctuator) or (source_code[i] in escape_seq):
-##            if(source_code=='"') or (source_code=="'"):
-##                words.append(lexem)
-##                lexem=""
-            if lexem=="":
+            if source_code[i]=='"': #0-9 while source_code[j]=='"': chk_str=source_code[j]+source_code[j+1] j=j+1
+
+                j=i+1
+                chk_str1=source_code[i]
+
+                while (source_code[j]!='"'):
+                    if (source_code[j] == '\n'):
+                        break
+                    #print('j=',j,'chk_str=',chk_str1)
+                    chk_str1=chk_str1+source_code[j]
+                    j=j+1
+                if (source_code[j] == '\n'):
+                    chk_str1=chk_str1+source_code[j]
+                    i=j
+                    chk_str1=chk_str1[:-1]
+                    words.append(chk_str1)
+                
+                else:
+                    chk_str1=chk_str1+source_code[j]
+                    i=j+1
+                    words.append(chk_str1)
+
+            elif source_code[i]=="'": #0-9 while source_code[j]=='"': chk_str=source_code[j]+source_code[j+1] j=j+1
+
+                j=i+1
+                chk_str1=source_code[i]
+                
+                while (source_code[j]!="'"):
+                    if (source_code[j] == '\n'):
+                        break
+                    #print('j=',j,'chk_str=',chk_str1)
+                    chk_str1=chk_str1+source_code[j]
+                    j=j+1
+                    
+                if (source_code[j] == '\n'):
+                    chk_str1=chk_str1+source_code[j]
+                    i=j
+                    chk_str1=chk_str1[:-1]
+                    words.append(chk_str1)
+                
+                else:
+                    chk_str1=chk_str1+source_code[j]
+                    i=j+1
+                    words.append(chk_str1)
+            
+            elif lexem=="":
                 words.append(source_code[i])
                 i=i+1
             else:
@@ -113,45 +449,67 @@ def word_splitter(source_code):
                 #print(i,words,"pun_esc")
             
         elif(source_code[i] in comment):
+            
             if lexem=="":
-                words.append(source_code[i])
-                i=i+1
+                j=i+1
+                chk_str2=source_code[i]
+                
+                while(source_code[j] !='\n' ):
+                    chk_str2=chk_str2+source_code[j]
+                    j=j+1
+                chk_str2=chk_str2+source_code[j]
+                i=j
+                chk_str2=chk_str2[:-1]
+                words.append(chk_str2)
+        
             else:
                 words.append(lexem)
-                words.append(source_code[i])
                 lexem=""
-                i=i+1
-                #print(i,words,"comm")
+                
+                j=i+1
+                chk_str2=source_code[i]
+                
+                while(source_code[j] !='\n' ):
+                    chk_str2=chk_str2+source_code[j]
+                    j=j+1
+                chk_str2=chk_str2+source_code[j]
+                i=j
+                chk_str2=chk_str2[:-1]
+                words.append(chk_str2)
+                
         else:
             lexem+=source_code[i]
             i+=1
             #print(i,words,"norm")
     return words
 
-def tokenizer(words):
-    tokens=[]
-    
-    for i in range(len(words)):
-            
     
 source_code = """
 int run = 50
 if(run >= 50):
-    print("King karlega, Bobsie the king karlega!")
+    print("King karlega, 'Bobsie the king' karlega!")
 else:
-    print("Naaaaa!")
+    print('"Naaaaa!"
+    ')
 while(a>6):
     print("ye kia baat hui boss?")
 for i in range(67):
     print("hey")
 #bobsie the king
 """
-
+print(source_code)
 words=word_splitter(source_code)
-
-print(word_splitter(source_code))
+tokenizer=tokenizer(words)
+for token in tokenizer:
+    print(token)
+#print(words)
+##for word in words:
+##    print(word)
 
 exit()
+
+
+
 ##import re
 ##
 ### Defining token types dictionary
